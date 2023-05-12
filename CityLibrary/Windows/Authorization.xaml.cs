ba37a77824fd950f2d781e3bbdfc47be71fab91c
Sofia <sofia_p_y@mail.ru>
2023-05-12 23:05:43 +0900

    Сделала автрозацию

--- CityLibrary/Windows/Authorization.xaml.cs
+++ CityLibrary/Windows/Authorization.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
 
 namespace CityLibrary
 {
@@ -20,9 +21,43 @@ namespace CityLibrary
     /// </summary>
     public partial class Authorization : Window
     {
+        CityLibraryEntities _entities = new CityLibraryEntities();
         public Authorization()
         {
             InitializeComponent();
+        }
+
+        private void SignInBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string userLogin = LoginTxt.Text;
+            string userPass = PassTxt.Password;
+
+            User user = _entities.User.FirstOrDefault(u => u.UserLogin == userLogin && u.UserPassword == userPass);
+            if(user != null)
+            {
+                string role;
+                switch (user.UserRole)
+                {
+                    case 1:
+                        role = "Зав. библиотекой";
+                        MessageBox.Show("Зав. библиотекой");
+                        break;
+                    default:
+                        role = "Библиотекарь";
+                        MessageBox.Show("Библиотекарь");
+                        break;
+                }
+            }
+            else
+                MessageBox.Show("Пользователь не найден!");
+        }
+
+        private void ShowPass_Checked(object sender, RoutedEventArgs e)
+        {
+            if (ShowPass.IsChecked == false)
+            {
+                PassTxt.PasswordChar = '*'; // скрыть пароль
+            }
         }
     }
 }
