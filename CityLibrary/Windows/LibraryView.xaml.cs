6981fbdfa33ce92573db60abe3b0b0f57c7d2414
Sofia <sofia_p_y@mail.ru>
2023-05-13 00:08:44 +0900

    Добавила новую форму для отображения данных

---
+++ CityLibrary/Windows/LibraryView.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CityLibrary.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для LibraryView.xaml
+    /// </summary>
+    public partial class LibraryView : Window
+    {
+        CityLibraryEntities _entities = new CityLibraryEntities();
+        public LibraryView()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            List<Book> books = _entities.Book.ToList();
+
+            dataGrid.ItemsSource = _entities.Book.ToList();
+
+
+        }
+    }
+}
